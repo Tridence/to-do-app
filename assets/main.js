daf3c3b107b4de044089e8932692b7bf98c8dc4a
tridence <brianwanjalabarasa@gmail.com>
2022-07-02 02:09:54 +0300

    Basic js structure done

---
+++ assets/main.js
@@ -0,0 +1,38 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//event listeners
+todoButton.addEventListener("click", addTodo);
+
+//functions
+function addTodo(event){ 
+    //Prevent the form from submitting
+
+    event.preventDefault();
+    //Todo DiV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create LI
+    const newTodo= document.createElement('li');
+    newTodo.innerText ='Hey!';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Checkmark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class= "fas fa-check"></i>'; 
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton); 
+
+     //Trash Button
+     const trashButton = document.createElement('button');
+     trashButton.innerHTML = '<i class= "fas fa-trash"></i>'; 
+     trashButton.classList.add("complete-btn");
+     todoDiv.appendChild(trashButton); 
+
+     //APPEND to list
+     todoList.appendChild(todoDiv)
+}
