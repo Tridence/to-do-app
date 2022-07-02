54ca2a2a3a92044651030feec3f72207af545e44
tridence <brianwanjalabarasa@gmail.com>
2022-07-02 11:00:04 +0300

    Initiated delete button

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,14 @@
 //selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 //event listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 
 //functions
-function addTodo(event){ 
+function addTodo(event) { 
     //Prevent the form from submitting
 
     event.preventDefault();
@@ -19,23 +19,23 @@ function addTodo(event){
     //Create LI
     const newTodo= document.createElement('li');
     newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
+    newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
 
     //Checkmark Button
-    const completedButton = document.createElement('button');
+    const completedButton = document.createElement("button");
     completedButton.innerHTML = '<i class= "fas fa-check"></i>'; 
     completedButton.classList.add("complete-btn");
     todoDiv.appendChild(completedButton); 
 
      //Trash Button
-     const trashButton = document.createElement('button');
+     const trashButton = document.createElement("button");
      trashButton.innerHTML = '<i class= "fas fa-trash"></i>'; 
      trashButton.classList.add("trash-btn");
      todoDiv.appendChild(trashButton); 
 
      //APPEND to list
-     todoList.appendChild(todoDiv)
+     todoList.appendChild(todoDiv);
 
      //Clear Input Value
      todoInput.value ="";
@@ -43,6 +43,16 @@ function addTodo(event){
 
 function deleteCheck(e){
     const item =e.target;
-    if(item.classList[0] == 'trash-btn');
-    todoList.remove();
+    //Delete ToDO
+    //console.log(e.target);
+    if(item.classList[0] == 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+//Checkmark
+if(item.classList[0] == "complete-btn"){
+    const todo = item.parentElement;
+    todo.classList.toggle('completed');
+}
 }
