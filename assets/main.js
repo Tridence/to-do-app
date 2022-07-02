233d09d9399e4bd495356d539ac3dda6f74f1d4c
tridence <brianwanjalabarasa@gmail.com>
2022-07-02 11:28:32 +0300

    Remove element animation

--- assets/main.js
+++ assets/main.js
@@ -47,7 +47,11 @@ function deleteCheck(e){
     //console.log(e.target);
     if(item.classList[0] == 'trash-btn'){
         const todo = item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
     }
 
 //Checkmark
